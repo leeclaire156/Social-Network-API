f1ba201f678991e2cd8119240cfc75d5c9796e30
lee.claire156 <lee.claire156@gmail.com>
2023-03-21 19:06:55 -0400

    Adds routes for users, friends, thoughts, and reactions

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/users
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/users/:userId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').post(createReaction).delete(deleteReaction);
+
+module.exports = router;
