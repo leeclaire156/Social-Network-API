b2cfbd3037f9d8b991cba8cda4993b7204087ba1
lee.claire156 <lee.claire156@gmail.com>
2023-03-22 00:14:16 -0400

    Adds CRUD operations for User

---
+++ controllers/userController.js
@@ -0,0 +1,53 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // Get all users
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Get a single user
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // create a new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json({ message: "User updated!", user })
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // Delete a user (bonus: and associated thoughts)
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ message: 'User deleted!' }))
+            .catch((err) => res.status(500).json(err));
+    },
+    addFriend(req, res) { },
+    deleteFriend(req, res) { },
+};
