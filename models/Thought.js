4cc8ffa1b6121ea7773e29fda26d43d9bfccdfc6
lee.claire156 <lee.claire156@gmail.com>
2023-03-21 20:31:20 -0400

    Adds User, Thought, and Reaction schemas

---
+++ models/Thought.js
@@ -0,0 +1,78 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: [280, 'Reached max character length']
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: format_date,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: [1, 'Thought text required'],
+            maxLength: [280, 'Reached max character length']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: format_date,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+        // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+function format_date(date) {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+}
+
+// Create a virtual property `reactionCount` that gets the length of the thought's reactions array field on query
+thoughtSchema.virtual('reactionCount ')
+    .get(function () {
+        return this.reactions.length;
+    })
+
+// Initialize our User model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: function (email) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
+                },
+                message: email => `${email.value} is not a valid email!`
+            },
+            required: [true, 'User email required']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+        // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual property `friendCount` that gets the length of the user's friends array field on query
+userSchema
+    .virtual('friendCount ')
+    .get(function () {
+        return this.friends.length;
+    })
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
